f690e44ef480693f6a38518a42a89a18c220fa28
loai-cpu3 <loai20189@gmail.com>
2025-04-14 23:00:12 +0200

    database update

--- Taswiya/Models/SupplierPaymentMethod.cs
+++ Taswiya/Models/SupplierPaymentMethod.cs
@@ -3,7 +3,7 @@
     public class SupplierPaymentMethod
     {
         
-        public int SupplierID { get; set; }
+        public String SupplierID { get; set; }
         public int PaymentMethodID { get; set; }
         public Supplier? Supplier { get; set; }
         public PaymentMethod? PaymentMethod { get; set; }
--- Taswiya/obj/Debug/net8.0/ConnectChain.AssemblyInfo.cs
+++ Taswiya/obj/Debug/net8.0/ConnectChain.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectChain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe5ed6737c6027428429a9ef762b0474b04d931")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+694df8ea3c9164da8b706fc70f1d89fc7c62cd18")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectChain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectChain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
