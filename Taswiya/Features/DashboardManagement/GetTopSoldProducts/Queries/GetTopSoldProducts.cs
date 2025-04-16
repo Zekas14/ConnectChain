e9552aedca63eb48f0300885bbaf342364a2197c
loai-cpu3 <loai20189@gmail.com>
2025-04-17 00:38:31 +0200

    GetTopSoldProducts and GetRevenueChart modifications

--- Taswiya/Features/DashboardManagement/GetTopSoldProducts/Queries/GetTopSoldProducts.cs
+++ Taswiya/Features/DashboardManagement/GetTopSoldProducts/Queries/GetTopSoldProducts.cs
@@ -3,17 +3,18 @@ using ConnectChain.Models;
 using ConnectChain.ViewModel.Dashboard.GetTopSoldProducts;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
 namespace ConnectChain.Features.DashboardManagement.GetTopSoldProducts.Queries
 {
-    public record GetTopSoldProductsQuery(string SupplierId, int Year, int Month, int Limit) : IRequest<IReadOnlyList<GetTopSoldProductsResponseViewModel>>;
+    public record GetTopSoldProductsQuery(string SupplierId, int Year, int Month, int Limit) : IRequest<ICollection<GetTopSoldProductsResponseViewModel>>;
 
     public class GetTopSoldProductsQueryHandler(IRepository<Order> repository)
-        : IRequestHandler<GetTopSoldProductsQuery, IReadOnlyList<GetTopSoldProductsResponseViewModel>>
+        : IRequestHandler<GetTopSoldProductsQuery, ICollection<GetTopSoldProductsResponseViewModel>>
     {
         private readonly IRepository<Order> repository = repository;
 
-        public async Task<IReadOnlyList<GetTopSoldProductsResponseViewModel>> Handle(GetTopSoldProductsQuery request, CancellationToken cancellationToken)
+        public async Task<ICollection<GetTopSoldProductsResponseViewModel>> Handle(GetTopSoldProductsQuery request, CancellationToken cancellationToken)
         {
             var orders = await repository.Get(o => o.SupplierId == request.SupplierId 
                                                    && o.CreatedDate.Year == request.Year 
@@ -31,12 +32,24 @@ namespace ConnectChain.Features.DashboardManagement.GetTopSoldProducts.Queries
                                         {
                                             ProductId = g.Key.ID,
                                             ProductName = g.Key.Name,
-                                            ImageUrls = g.Key.Images.Select(img => img.Url).ToList(),
+                                            ImageUrl = GetValidImageUrl(g.Key.Images),
                                             TotalSoldQuantity = g.Sum(oi => oi.Quantity)
                                         })
                                         .ToList();
 
             return topSoldProducts;
+        }
+        private string GetValidImageUrl(ICollection<Image> images)
+        {
+            if (images == null || !images.Any())
+            {
+                return "https://res.cloudinary.com/dt5cgz1ly/image/upload/v1744829375/l0o8hqxbgo9fweubwfbj.jpg";
+            }
+            var regexPattern = @"^http:\/\/res\.cloudinary\.com\/dt5cgz1ly\/image\/upload\/v\d+\/[a-zA-Z0-9]+\.jpg$";
+            var regex = new Regex(regexPattern);
+
+            var validImage = images.FirstOrDefault(img => img.Url != null && regex.IsMatch(img.Url));
+            return validImage?.Url ?? "https://res.cloudinary.com/dt5cgz1ly/image/upload/v1744829375/l0o8hqxbgo9fweubwfbj.jpg";
         }
     }
 }
