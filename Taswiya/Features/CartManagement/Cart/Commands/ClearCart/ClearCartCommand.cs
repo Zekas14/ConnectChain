592a9285451c79d48ddef1d9681a6453f453ae50
Ahmed Zakarya <ahmedzrks123@gmail.com>
2025-06-11 23:27:24 +0300

    Clear Cart Endpoint

--- Taswiya/Features/CartManagement/Cart/Commands/ClearCart/ClearCartCommand.cs
+++ Taswiya/Features/CartManagement/Cart/Commands/ClearCart/ClearCartCommand.cs
@@ -1,6 +1,32 @@
-﻿namespace ConnectChain.Features.CartManagement.Cart.Commands.ClearCart
+﻿using ConnectChain.Data.Repositories.Repository;
+using ConnectChain.Helpers;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConnectChain.Features.CartManagement.Cart.Commands.ClearCart
 {
-    public class ClearCartCommand
+    public record ClearCartCommand(string CustomerId) : IRequest<RequestResult<bool>>;
+
+    public class ClearCartCommandHandler(IRepository<Models.Cart> repository, ICacheService cache)
+        : IRequestHandler<ClearCartCommand, RequestResult<bool>>
     {
+        private readonly IRepository<Models.Cart> repository = repository;
+        private readonly ICacheService cache = cache;
+
+        public async Task<RequestResult<bool>> Handle(ClearCartCommand request, CancellationToken cancellationToken)
+        {
+            var cart = repository.Get(c => c.CustomerId == request.CustomerId)
+                .Include(c => c.Items)
+                .FirstOrDefault();
+
+            if (cart == null)
+                return RequestResult<bool>.Failure(ErrorCode.NotFound, "Cart not found.");
+
+            cart.Items.Clear();
+            await repository.SaveChangesAsync();
+
+            return RequestResult<bool>.Success(true, "Cart cleared.");
+        }
     }
+
 }
